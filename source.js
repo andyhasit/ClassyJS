0495ba6c863f21aabd0181a4cb702dea0f458b67
Andrew Buchan <andyhasit@gmail.com>
2016-01-26 16:27:54 +0000

    __super__ is broken

--- source.js
+++ source.js
@@ -12,8 +12,41 @@ function cls(definition) {
   Template.prototype.toString = function() {
     return '<A ClassyJS object. Try setting a toString(me) function in the class definition.>';
   };
+  Template.prototype.__init__ = function() {
+    c.log('init');
+    c.log(this);
+    c.log(arguments[0]);
+    return definition.__constructor__.apply(this, modifyArgs(this, arguments));
+  };
   if (definition.hasOwnProperty('__inherits__')) {
-    Template.prototype = new definition['__inherits__'];
+    var parentDefinition = new definition.__inherits__;
+    Template.prototype = parentDefinition;
+    //Template.prototype.__super__ = parentDefinition;
+    var __super__ = {};
+    for(prop in parentDefinition) {
+      __super__[prop] = parentDefinition[prop];
+    }
+    //c.log(__super__);
+    //__super__.constructor = parentDefinition.__init__;
+    Template.prototype.__super__ = __super__;
+    /*
+    Option 1:
+      allows me to call me.__super__.constructor()
+      calling me.__super.foo() will fail if foo is defined on parent as well as overriden.
+    
+    
+    Template.prototype.__super__ = {
+      //constructor: function(){ parentDefinition.apply(this)}
+      __init__: function(){
+          c.log(this);
+          parentDefinition.constructor.apply(this, modifyArgs(it, arguments))
+      }
+    };
+    for(prop in parentDefinition) {
+      //c.log(prop);
+      Template.prototype.__super__[prop] = prop;
+    }
+    */
   }
   for (var property in definition) {
     if ((definition.hasOwnProperty(property)) && !(/^__.*__/.exec(property))) {
--- tests.js
+++ tests.js
@@ -13,10 +13,8 @@ function prnt(desc, obj) {
 describe('ClassyJS', function() {
   
   it('objects are created with correct properties', function() {
-    Person = cls({
+    var Person = cls({
       __constructor__: function(me, firstname, lastname, age) {
-        prnt('me', me);
-        prnt('firstname', firstname);
         me.firstname = firstname;
         me.lastname = lastname;
         me.age = age;
@@ -25,13 +23,220 @@ describe('ClassyJS', function() {
         return me.firstname + ' ' + me.lastname + ' ' + me.age;
       }
     });
-    person = new Person('Mickey', 'Mouse', 100);
+    var person = new Person('Mickey', 'Mouse', 100);
     
     expect(person.age).toEqual(100);
     expect(person.firstname).toEqual('Mickey');
     expect(person.lastname).toEqual('Mouse');
     expect(person.getDetailsString()).toEqual('Mickey Mouse 100');
   });
+  
+  it('method arguments are passed correctly.', function() {
+    var Person = cls({
+      __constructor__: function(me) {
+      },
+      returnStuff: function(me, x, y, z) {
+        return [x, y, z];
+      }
+    });
+    var person = new Person();
+    var result = person.returnStuff(34, 78);
+    expect(result[0]).toEqual(34);
+    expect(result[1]).toEqual(78);
+    expect(result[2]).toEqual(undefined);
+  });
+  
+  it('subclass inherits methods, and parameters are passed correctly.', function() {
+    var Person = cls({
+      __constructor__: function(me) {
+      },
+      returnStuff: function(me, x, y, z) {
+        return [x, y, z];
+      }
+    });
+    
+    var SpecialPerson = cls({
+      __constructor__: function(me) {
+      },
+      __inherits__: Person
+    });
+    
+    var person = new SpecialPerson();
+    var result = person.returnStuff(34, 78);
+    expect(result[0]).toEqual(34);
+    expect(result[1]).toEqual(78);
+    expect(result[2]).toEqual(undefined);
+  });
+  
+  it('subclass can access parent method directly', function() {
+    var Parent = cls({
+      __constructor__: function(me) {
+      },
+      parentFoo: function(me, x, y) {
+        return x + y;
+      }
+    });
+    
+    var Child = cls({
+      __constructor__: function(me) {
+      },
+      __inherits__: Parent,
+      childFoo: function(me, x, y) {
+        return me.parentFoo(x, y);
+      }
+    });
+    
+    var child = new Child();
+    expect(child.childFoo(6, 10)).toEqual(16);
+  });
+  
+    it('parent method accessed directly refer to the correct me', function() {
+    var Parent = cls({
+      __constructor__: function(me) {
+        me.start = 100;
+      },
+      parentFoo: function(me, x, y) {
+        c.log('foo');
+        return me.start + x + y;
+      }
+    });
+    
+    var Child = cls({
+      __constructor__: function(me) {
+        me.start = 200;
+      },
+      __inherits__: Parent,
+      childFoo: function(me, x, y) {
+        return me.parentFoo(x, y);
+      }
+    });
+    
+    var child = new Child();
+    expect(child.childFoo(6, 10)).toEqual(216);
+  });
+  
+  it('subclass can access parent method via __super__', function() {
+    var Parent = cls({
+      __constructor__: function(me) {
+      },
+      foo: function(me, x, y) {
+        c.log('foo');
+        return x + y;
+      }
+    });
+    
+    var Child = cls({
+      __inherits__: Parent,
+      __constructor__: function(me) {
+      },
+      bar: function(me, x, y) {
+        return me.__super__.foo(x, y);
+      }
+    });
+    var child = new Child();
+    expect(child.bar(6, 10)).toEqual(16);
+  });
+  
+  it('method called via __super__ refers to correct me', function() {
+    var Parent = cls({
+      __constructor__: function(me) {
+        me.start = 100;
+      },
+      foo: function(me, x, y) {
+        c.log('foo');
+        return me.start + x + y;
+      }
+    });
+    
+    var Child = cls({
+      __inherits__: Parent,
+      __constructor__: function(me) {
+        me.start = 200;
+      },
+      bar: function(me, x, y) {
+        return me.__super__.foo(x, y);
+      }
+    });
+    var child = new Child();
+    expect(child.bar(6, 10)).toEqual(216);
+  });
+    
+  it("subclass method override doesn't affect parent method", function() {
+    var Parent = cls({
+      __constructor__: function(me) {
+      },
+      foo: function(me) {
+        return 'parent stuff';
+      }
+    });
+    
+    var Child = cls({
+      __constructor__: function(me) {
+      },
+      __inherits__: Parent,
+      foo: function(me) {
+        return 'child stuff';
+      }
+    });
+    
+    var parent = new Parent();
+    var child = new Child();
+    expect(child.foo()).toEqual('child stuff');
+    expect(parent.foo()).toEqual('parent stuff');
+  });
+  
+  it('subclass can access parent constructor', function() {
+    var Parent = cls({
+      __constructor__: function(me, name) {
+        me.name = name;
+        c.log('Called Parent with ' + name);
+      },
+      sayHi: function(me) {
+        c.log('hi');
+      }
+    });
+    
+    var Child = cls({
+      __inherits__: Parent,
+      __constructor__: function(me, firstname, lastname) {
+        me.firstname = firstname;
+        me.lastname = lastname;
+        me.__super__.__init__(firstname + ' ' + lastname);
+      },
+    });
+    
+    var child = new Child('Mickey', 'Mouse');
+    expect(child.name).toEqual('Mickey Mouse');
+  });
+  
+  it('subclass can access overriden parent method.', function() {
+    var Parent = cls({
+      __constructor__: function(me) {
+      },
+      foo: function(me) {
+        return 'parent stuff';
+      }
+    });
+    
+    var Child = cls({
+      __constructor__: function(me) {
+      },
+      __inherits__: Parent,
+      foo: function(me) {
+        return 'child stuff ' + me.__super__.foo();
+      }
+    });
+    
+    var parent = new Parent();
+    var child = new Child();
+    expect(child.foo()).toEqual('child stuff parent stuff');
+    expect(parent.foo()).toEqual('parent stuff');
+  });
+  
+  /*
+  
+  
+  */
   
   
 });
